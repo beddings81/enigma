a240e7505dca413a5de13174aa33e8374076f9ae
Braxton Eddings <beddings81@gmail.com>
2022-11-10 13:14:19 -0600

    Add shifter test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,10 +12,13 @@ RSpec.describe Enigma do
   end
 
   it 'can slit a message' do
-    expect(enigma.split("hello")).to eq(["h", "e", "l", "l", "o"])
+    expect(enigma.spliter("hello")).to eq(["h", "e", "l", "l", "o"])
   end
 
-  # it 'can encrypt a message with a key and date' do
-  #   expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
-  # end
+  it 'can take a date and keys to create the shift' do
+    expect(enigma.shifter("040895", "02715")).to eq(0)
+
+  it 'can encrypt a message with a key and date' do
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
 end
