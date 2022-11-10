fa7a52c66cbee43b7e0880bd2bbab2b6a9d3971f
Braxton Eddings <beddings81@gmail.com>
2022-11-10 13:45:32 -0600

    Start shift method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,10 @@
 class Enigma
 
-  def encrypt(message, key, date)  
-    split = spliter(message)
-    split
-  end
+  # def encrypt(message, key, date)  
+  #   split = spliter(message)
+  #   offset = offseter(date)
+  #   # require 'pry'; binding.pry
+  # end
 
   def character_set
     set = ("a".."z").to_a
@@ -14,15 +15,17 @@ class Enigma
     string.split("")
   end
 
-  def offset(date)
+  def offseter(date)
     x = date.to_i
     y = x * x
     z = y.to_s[-4..-1].to_i
     z
   end
 
-  # def shifter(date, key)
-  # end
+  def shift(keys, offsets)
+    shift_hash = {:A => 0, :B => 0, :C => 0, :D => 0}
+    require 'pry'; binding.pry
+  end
 
 
 end
