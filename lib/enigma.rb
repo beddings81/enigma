9f42dccc63492efb532db4d472fa6fa07d217747
Braxton Eddings <beddings81@gmail.com>
2022-11-10 17:16:53 -0600

    Pass updated offseter test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -16,15 +16,23 @@ class Enigma
   end
 
   def offseter(date)
-    x = date.to_i
-    y = x * x
-    z = y.to_s[-4..-1].to_i
-    z
+    date_num = date.to_i
+    date_squared = date_num * date_num
+    last_4 = date_squared.to_s[-4..-1]
+    offset_strings = last_4.split("")
+     offsets = offset_strings.map do |num|
+      num.to_i
+    end
+    offsets
   end
 
   def shift(keys, offsets)
     shift_hash = {:A => 0, :B => 0, :C => 0, :D => 0}
-    require 'pry'; binding.pry
+    shift_hash[:A] += keys[0] + offsets[0]
+    shift_hash[:B] += keys[1] + offsets[1]
+    shift_hash[:C] += keys[2] + offsets[2]
+    shift_hash[:D] += keys[3] + offsets[3]
+    shift_hash
   end
 
 
