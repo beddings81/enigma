63e6999e0c06615c8c4d718125a05dc008f6ce19
Braxton Eddings <beddings81@gmail.com>
2022-11-10 12:49:56 -0600

    Add encrypt with key and date test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Enigma do
     expect(enigma.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 
-  # it 'can encrypt a message with a key and date' do
-  #   expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
-  # end
+  it 'can encrypt a message with a key and date' do
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
 end
