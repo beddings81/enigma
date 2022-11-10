2cd08373c644087c6b538893ecdebd4c29a9f0f2
Braxton Eddings <beddings81@gmail.com>
2022-11-10 13:07:16 -0600

    Pass split test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,22 @@
 class Enigma
 
   # def encrypt(message, key, date)
-
+  #   if key.nil?
+  #     #do method
+  #   elsif date.nil?
+  #     date = DateTime.now(strftime("%d%m%y"))
+  #   else 
+  #     messgae.
+  #   end
   # end
 
   def character_set
     set = ("a".."z").to_a
     set.push(" ")
+  end
+
+  def split(string)
+    string.split("")
   end
 
 end
