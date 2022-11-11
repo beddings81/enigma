08d70821a2c9dd11e6cfbe137dffc6df6acf9b03
Braxton Eddings <beddings81@gmail.com>
2022-11-11 15:30:25 -0600

    Pass numbers test

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,10 +1,11 @@
 require 'date'
 
 class Shift
-  attr_reader :key
+  attr_reader :key, :numbers
 
   def initialize
     @key = ""
+    @numbers = ["0","1","2","3","4","5","6","7","8","9"]
   end
 
   def set_date(date=nil)
@@ -13,5 +14,9 @@ class Shift
     else 
       (date.to_i ** 2).to_s[-4..-1]
     end
+  end
+
+  def rand_key
+    @numbers.sample(5).join
   end
 end
