8b53d1fdf04d062f22c2d8e9045590373bae0f31
Braxton Eddings <beddings81@gmail.com>
2022-11-11 15:30:05 -0600

    Add numbers array test

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,2 +1,17 @@
+require 'date'
+
 class Shift
+  attr_reader :key
+
+  def initialize
+    @key = ""
+  end
+
+  def set_date(date=nil)
+    if date.nil?
+      (DateTime.now.strftime('%d%m%y').to_i ** 2).to_s[-4..-1]
+    else 
+      (date.to_i ** 2).to_s[-4..-1]
+    end
+  end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe Shift do
   it 'exist and has attributes' do
     expect(shift).to be_a(Shift)
     expect(shift.key).to eq("")
+    expect(shift.numbers).to eq(["0","1","2","3","4","5","6","7","8","9"])
   end
 
   it 'can take a date and create the offsets' do
     expect(shift.set_date("040895")).to eq("1025")
     expect(shift.set_date).to eq("8884")
   end
+
+  it 'can generate a random 5 digit key' do
+    expect(shift.rand_key).to be_a(String)
+  end
 end
+
