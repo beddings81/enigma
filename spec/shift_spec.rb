dcb0d15fc799da5d445b912572199d87c5dbba52
Braxton Eddings <beddings81@gmail.com>
2022-11-11 14:55:27 -0600

    Add set date test to shift class

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Shift do
 
   it 'exist and has attributes' do
     expect(shift).to be_a(Shift)
+    expect(shift.key).to eq("")
+  end
+
+  it 'can take a date and create the offsets' do
+    expect(shift.set_date("040895")).to eq([1,0,2,5])
+    expect(shift.set_date.to eq([8,8,8,4])
   end
 end
