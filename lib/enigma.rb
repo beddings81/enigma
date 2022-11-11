19925e385ca9b659b6b6e53fa1ec40bd64d77097
Braxton Eddings <beddings81@gmail.com>
2022-11-11 15:44:35 -0600

    Pass set key method and pass test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,49 +1,19 @@
 class Enigma
 
   def encrypt(message, key, date)  
-    spliter(message)
-    offseter(date)
-    set_key(key)
+    spliter(message) #message is formated
+    offseter(date)   #date is formated
+    set_key(key)     #key is formatted
     # require 'pry'; binding.pry
   end
 
-  def character_set
+  def characters
     set = ("a".."z").to_a
     set.push(" ")
   end
 
   def spliter(string)
-    string.split("")
-  end
-
-  def offseter(date)
-    date_num = date.to_i
-    date_squared = date_num * date_num
-    last_4 = date_squared.to_s[-4..-1]
-    offset_strings = last_4.split("")
-     offsets = offset_strings.map do |num|
-      num.to_i
-    end
-    offsets
-  end
-
-  def set_key(key)
-    key_array = []
-    x = key.split("")
-    a = x[0..1].join.to_i
-    b = x[1..2].join.to_i
-    c = x[2..3].join.to_i
-    d = x[3..4].join.to_i
-    key_array << a
-    key_array << b
-    key_array << c
-    key_array << d
-    key_array
-    # require 'pry'; binding.pry
-  end
-
-  def key_hash
-    key_hash = {:A => a, :B => b, :C => c, :D => d}
+    string.chars
   end
 
   def shift(keys, offsets)
@@ -54,4 +24,20 @@ class Enigma
     shift_hash[:D] += keys[3] + offsets[3]
     shift_hash
   end
+
+  def cypher(message,keys)
+    keys = keys.values
+    encrypted = ""
+    ma = spliter(message)
+    until encrypted.length == ma.length do
+        encrypted << characters[(characters.index(ma[0]) + keys[0]) %27]
+        encrypted << characters[(characters.index(ma[1]) + keys[1]) %27]
+        encrypted << characters[(characters.index(ma[2]) + keys[2]) %27]
+        encrypted << characters[(characters.index(ma[3]) + keys[3]) %27]
+      # require 'pry'; binding.pry
+    end
+    encrypted
+  end
+
 end
+
--- lib/shift.rb
+++ lib/shift.rb
@@ -8,7 +8,7 @@ class Shift
     @numbers = ["0","1","2","3","4","5","6","7","8","9"]
   end
 
-  def set_date(date=nil)
+  def set_offset(date=nil)
     if date.nil?
       (DateTime.now.strftime('%d%m%y').to_i ** 2).to_s[-4..-1]
     else 
@@ -18,5 +18,9 @@ class Shift
 
   def rand_key
     @numbers.sample(5).join
+  end
+
+  def set_key(key)
+    {:A => key[0..1], :B => key[1..2], :C => key[2..3],:D => key[3..4]}
   end
 end
