e60220026a2b236a09c224f4fd8b11f183192662
Braxton Eddings <beddings81@gmail.com>
2022-11-12 11:41:58 -0600

    Add decrypt test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,4 +11,8 @@ RSpec.describe Enigma do
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq("{encryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
     expect(enigma.encrypt("HELLO WORLD", "02715", "040895")).to eq("{encryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
   end
+
+  it 'can decrypt a message with a key and date' do
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq("{decryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
+    # expect(enigma.decrypt("KEDER OHULW", "02715", "040895")).to eq("{decryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
 end
