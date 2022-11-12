a0b5006f66e282d95cdf6d4791e7989f70ef3176
Braxton Eddings <beddings81@gmail.com>
2022-11-11 19:00:06 -0600

    Pass cipher test

--- lib/rotation.rb
+++ lib/rotation.rb
@@ -10,7 +10,42 @@ class Rotation
     string.chars
   end
 
-  def spin(letter, shift)
+  def shift(letter, shift)
     letters[(letters.find_index(letter) + shift) % 27]
   end
+
+  def cipher(shifts)
+    counter = 0
+    encrypted = ""
+    string.each_char do |char|
+      if counter == 0
+        shifted = letters.rotate(shifts[:A])
+        index_position = letters.find_index(char)
+        encrypted += shifted[index_position]
+        counter += 1
+      elsif counter == 1
+        shifted = letters.rotate(shifts[:B])
+        index_position = letters.find_index(char)
+        encrypted += shifted[index_position]
+        counter += 1
+      elsif counter == 2
+        shifted = letters.rotate(shifts[:C])
+        index_position = letters.find_index(char)
+        encrypted += shifted[index_position]
+        counter += 1
+      elsif counter == 3
+        shifted = letters.rotate(shifts[:D])
+        index_position = letters.find_index(char)
+        encrypted += shifted[index_position]
+        counter = 0
+      end
+    end
+    encrypted
+  end
 end
+
+        # shifted_alphabet = letters.rotate(find_shift[:a])
+        # index_position = alphabet.find_index(letter)
+        # encrypted += shifted_alphabet[index_position]
+        # counter += 1
+        #counter resets to go back through the string
