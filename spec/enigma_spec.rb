5d32a9a8a515ba01b5c83139cdb290681199d50d
Braxton Eddings <beddings81@gmail.com>
2022-11-12 20:41:47 -0600

    Update encrypt and decrypt test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe Enigma do
   end
 
   it 'can encrypt a message with a key and date' do
-    expect(enigma.encrypt("hello world", "02715", "040895")).to eq("{encryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
-    expect(enigma.encrypt("HELLO WORLD", "02715", "040895")).to eq("{encryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({:date=>"040895", :encryption=>"keder ohulw", :key=>"02715"})
+    expect(enigma.encrypt("HELLO WORLD", "02715", "040895")).to eq({:date=>"040895", :encryption=>"keder ohulw", :key=>"02715"})
   end
 
+
   it 'can decrypt a message with a key and date' do
-    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq("{decryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
-    # expect(enigma.decrypt("KEDER OHULW", "02715", "040895")).to eq("{decryption: => rmjdyhugatb,\n      key:        => 02715,\n      date:       => 040895\n    }")
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq({:date=>"040895", :encryption=>"hello world", :key=>"02715"})
+    expect(enigma.decrypt("KEDER OHULW", "02715", "040895")).to eq({:date=>"040895", :encryption=>"hello world", :key=>"02715"})
+  end
 end
