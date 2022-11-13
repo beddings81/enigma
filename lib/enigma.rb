eca36f4b86e24e5fd3ccec78c674cc56edd09d98
Braxton Eddings <beddings81@gmail.com>
2022-11-12 21:15:49 -0600

    Pass decrypt test with no given date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,7 +14,7 @@ class Enigma < Rotation
     }
   end
 
-  def decrypt(message, key, date)
+  def decrypt(message, key, date=today)
     the_keys = set_key(key)
     the_offsets = set_offset(date)
     the_shifts = find_shift(the_keys, the_offsets)
